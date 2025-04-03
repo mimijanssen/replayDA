018407f024a54e6060c6dad4a8fd0acb3998c1fb
mimijanssen <55414810+mimijanssen@users.noreply.github.com>
2025-04-03 17:01:54 -0400

    added cross correlation between fiber and LFP signal code

--- Analysis/linearmixedmodels_motivation.m
+++ Analysis/linearmixedmodels_motivation.m
@@ -2,7 +2,7 @@
 
 % subject, session, pre/post, SWR-DA, motivation 
 clear; clc;
-cd 'D:\Mouse_avg'
+cd 'F:\Mouse_avg'
 load ('colors.mat')
 
 matrix_valswr = zeros(86,5);
@@ -10,7 +10,7 @@ matrix_valswr = zeros(86,5);
 %%  Populate Matrix with Mouse Names 
 
 % ~~~~~~~~~~~~~~ SWR-DA ~~~~~~~~~~~~~~
-cd 'D:\M433\avg_data\avg_data'
+cd 'F:\M433\avg_data\avg_data'
 Files=dir('*.*');
 count_mouse = 1; 
 for k=3:length(Files)
@@ -24,7 +24,7 @@ for k=3:length(Files)
    count_mouse = count_mouse + 1;
 end
 
-cd 'D:\M453\avg_data\avg_data'
+cd 'F:\M453\avg_data\avg_data'
 Files=dir('*.*');
 for k=3:length(Files)
    FileNames=Files(k).name;
@@ -37,7 +37,7 @@ for k=3:length(Files)
    count_mouse = count_mouse + 1;
 end
 
-cd 'D:\M460\avg_data\avg_data'
+cd 'F:\M460\avg_data\avg_data'
 Files=dir('*.*');
 for k=3:length(Files)
    FileNames=Files(k).name;
@@ -50,7 +50,7 @@ for k=3:length(Files)
    count_mouse = count_mouse + 1;
 end
 
-cd 'D:\M533\avg_data\avg_data'
+cd 'F:\M533\avg_data\avg_data'
 Files=dir('*.*');
 for k=3:length(Files)
    FileNames=Files(k).name;
@@ -63,7 +63,7 @@ for k=3:length(Files)
    count_mouse = count_mouse + 1;
 end
 
-cd 'D:\M534\avg_data\avg_data'
+cd 'F:\M534\avg_data\avg_data'
 Files=dir('*.*');
 for k=3:length(Files)
    FileNames=Files(k).name;
@@ -76,7 +76,7 @@ for k=3:length(Files)
    count_mouse = count_mouse + 1;
 end
 
-cd 'D:\M545\avg_data\avg_data'
+cd 'F:\M545\avg_data\avg_data'
 Files=dir('*.*');
 for k=3:length(Files)
    FileNames=Files(k).name;
@@ -89,7 +89,7 @@ for k=3:length(Files)
    count_mouse = count_mouse + 1;
 end
 
-cd 'D:\M547\avg_data\avg_data'
+cd 'F:\M547\avg_data\avg_data'
 Files=dir('*.*');
 for k=3:length(Files)
    FileNames=Files(k).name;
@@ -102,7 +102,7 @@ for k=3:length(Files)
    count_mouse = count_mouse + 1;
 end
 
-cd 'D:\M548\avg_data\avg_data'
+cd 'F:\M548\avg_data\avg_data'
 Files=dir('*.*');
 for k=3:length(Files)
    FileNames=Files(k).name;
@@ -148,15 +148,16 @@ for s = 1:length(structure_names)
     for i = 1:num_sessions
         session = session_names{i}; % Get current session name
         first_half = floor(length(curr_structure.(session).avg_fiber_pre)/2);
-        
-        mu_pre = mean(curr_structure.(session).circ_avg_pre(first_half+1:end));
-        mu_post = mean(curr_structure.(session).circ_avg_post(first_half+1:end));
-        std_pre = mean(curr_structure.(session).circ_std_pre(first_half+1:end)); % should be/ 2
-        std_post = mean(curr_structure.(session).circ_std_post(first_half+1:end)); 
+
+        one_sec = first_half + 1000; % ok I hardcoded this
+        mu_pre = mean(curr_structure.(session).circ_avg_pre(first_half+1:one_sec));
+        mu_post = mean(curr_structure.(session).circ_avg_post(first_half+1:one_sec));
+        std_pre = mean(curr_structure.(session).circ_std_pre(first_half+1:one_sec)); 
+        std_post = mean(curr_structure.(session).circ_std_post(first_half+1:one_sec)); 
 
 
-        SWR_DA_strength.pre(1,i) = max((curr_structure.(session).avg_fiber_pre(first_half+1:end)-mu_pre)/std_pre);  % currently this is dividing by 2 sd. So I want to divide by one
-        matrix_valswr(count_mouse,4) = max((curr_structure.(session).avg_fiber_pre(first_half+1:end)-mu_pre)/std_pre); % pre is 1
+        SWR_DA_strength.pre(1,i) = max((curr_structure.(session).avg_fiber_pre(first_half+1:one_sec)-mu_pre)/std_pre);  % currently this is dividing by 2 sd. So I want to divide by one
+        matrix_valswr(count_mouse,4) = max((curr_structure.(session).avg_fiber_pre(first_half+1:one_sec)-mu_pre)/std_pre); % pre is 1
         count_mouse = count_mouse + 1;
         % did i z-score first for correlation plots?
         % can't compare the size of these two peaks because I'm z-scoring
@@ -164,20 +165,9 @@ for s = 1:length(structure_names)
         % the future. 
 
         % Post-condition SWR-DA strength
-        SWR_DA_strength.post(1,i) = max((curr_structure.(session).avg_fiber_post(first_half+1:end)-mu_post)/std_post);
-        matrix_valswr(count_mouse,4) =max((curr_structure.(session).avg_fiber_post(first_half+1:end)-mu_post)/std_post);
+        SWR_DA_strength.post(1,i) = max((curr_structure.(session).avg_fiber_post(first_half+1:one_sec)-mu_post)/std_post);
+        matrix_valswr(count_mouse,4) =max((curr_structure.(session).avg_fiber_post(first_half+1:one_sec)-mu_post)/std_post);
         count_mouse = count_mouse + 1;
-
-        % % Pre-condition SWR-DA strength
-        % % subtract the mean and divide by sd and then find the max value. 
-        % SWR_DA_strength.pre(1,i) = max((curr_structure.(session).avg_fiber_pre(first_half+1:end-(first_half/2))-curr_structure.(session).circ_avg_pre(first_half+1:end-(first_half/2)))/(curr_structure.(session).circ_std_pre(first_half+1:end-(first_half/2)))); 
-        % matrix_valswr(count_mouse,4) = max((curr_structure.(session).avg_fiber_pre(first_half+1:end-(first_half/2))-curr_structure.(session).circ_avg_pre(first_half+1:end-(first_half/2)))/(curr_structure.(session).circ_std_pre(first_half+1:end-(first_half/2)))); % pre is 1
-        % count_mouse = count_mouse + 1;
-        % 
-        % % Post-condition SWR-DA strength
-        % SWR_DA_strength.post(1,i) = max((curr_structure.(session).avg_fiber_post(first_half+1:end-(first_half/2))- curr_structure.(session).circ_avg_post(first_half+1:end-(first_half/2)))/(curr_structure.(session).circ_std_post(first_half+1:end-(first_half/2))));
-        % matrix_valswr(count_mouse,4) = max((curr_structure.(session).avg_fiber_post(first_half+1:end-(first_half/2))-curr_structure.(session).circ_avg_post(first_half+1:end-(first_half/2)))/(curr_structure.(session).circ_std_post(first_half+1:end-(first_half/2))));
-        % count_mouse = count_mouse + 1;
 
     end
     
@@ -187,56 +177,56 @@ end
 
 %% ~~~~~~~~~~~~~~ POS ~~~~~~~~~~~~~~
 
-cd 'D:\M433\avg_data\pos'
+cd 'F:\M433\avg_data\pos'
 Files=dir('*.*');
 for k=3:length(Files)
    FileNames=Files(k).name;
    M433speed.(['track_speed',num2str(k-2)]) = load(FileNames);
 end
 
-cd 'D:\M453\avg_data\pos'
+cd 'F:\M453\avg_data\pos'
 Files=dir('*.*');
 for k=3:length(Files)
    FileNames=Files(k).name;
    M453speed.(['track_speed',num2str(k-2)]) = load(FileNames);
 end
 
-cd 'D:\M460\avg_data\pos'
+cd 'F:\M460\avg_data\pos'
 Files=dir('*.*');
 for k=3:length(Files)
    FileNames=Files(k).name;
    M460speed.(['track_speed',num2str(k-2)]) = load(FileNames);
 end
 
-cd 'D:\M533\avg_data\pos'
+cd 'F:\M533\avg_data\pos'
 Files=dir('*.*');
 for k=3:length(Files)
    FileNames=Files(k).name;
    M533speed.(['track_speed',num2str(k-2)]) = load(FileNames);
 end
 
-cd 'D:\M534\avg_data\pos'
+cd 'F:\M534\avg_data\pos'
 Files=dir('*.*');
 for k=3:length(Files)
    FileNames=Files(k).name;
    M534speed.(['track_speed',num2str(k-2)]) = load(FileNames);
 end
 
-cd 'D:\M545\avg_data\pos'
+cd 'F:\M545\avg_data\pos'
 Files=dir('*.*');
 for k=3:length(Files)
    FileNames=Files(k).name;
    M545speed.(['track_speed',num2str(k-2)]) = load(FileNames);
 end
 
-cd 'D:\M547\avg_data\pos'
+cd 'F:\M547\avg_data\pos'
 Files=dir('*.*');
 for k=3:length(Files)
    FileNames=Files(k).name;
    M547speed.(['track_speed',num2str(k-2)]) = load(FileNames);
 end
 
-cd 'D:\M548\avg_data\pos'
+cd 'F:\M548\avg_data\pos'
 Files=dir('*.*');
 for k=3:length(Files)
    FileNames=Files(k).name;
